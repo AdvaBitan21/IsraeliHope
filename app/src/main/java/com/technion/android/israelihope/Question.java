156f84182558535422bba9fd1ab9626f1c07a78f
Adva Bitan <advoshb@gmail.com>
2020-04-13 20:59:44 +0300

    just a start

--- app/src/main/java/com/technion/android/israelihope/Question.java
+++ app/src/main/java/com/technion/android/israelihope/Question.java
@@ -1,4 +1,15 @@
 package com.technion.android.israelihope;
 
+import java.util.ArrayList;
+
 public class Question {
+    private String id;
+    private String content;
+    private Utils.QuestionType questionType;
+    private ArrayList<String> answers; // from 2 to 4
+    private String from_email;
+    private String to_email;
+    //private Religon religon_target need to add the religon the question discusses on
+    private ArrayList<Integer> countRights;
+    private int count_answers;
 }
--- app/src/main/java/com/technion/android/israelihope/Utils.java
+++ app/src/main/java/com/technion/android/israelihope/Utils.java
@@ -1,9 +1,9 @@
 package com.technion.android.israelihope;
 
 public class Utils {
-    enum Level {
-        LOW,
-        MEDIUM,
-        HIGH
+    enum QuestionType {
+        YesNo,
+        FourAnswers,
+        MultipleChoice
     }
 }
